e84425014a77ccd5a3008504ba6f8611d5f3915b
caran <carl.andersson@westermo.com>
2024-05-16 11:35:27 +0200

    Benchmarking and Github Actions

---
+++ benchmarks/Program.cs
@@ -0,0 +1,35 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using YangParser;
+using YangParser.SemanticModel;
+
+namespace Benchmarks;
+
+[JsonExporterAttribute.Full]
+[JsonExporterAttribute.FullCompressed]
+public class ParsingBenchmarks
+{
+    private string source;
+    private YangStatement statement;
+
+    [GlobalSetup]
+    public void Setup()
+    {
+        source = File.ReadAllText("../../../../lin.yang");
+        statement = Parser.Parse("lin.yang", source);
+    }
+
+    [Benchmark]
+    public YangStatement Parse() => Parser.Parse("lin.yang", source);
+
+    [Benchmark]
+    public IStatement SemanticModel() => StatementFactory.Create(statement);
+}
+
+internal static class Program
+{
+    private static void Main(string[] args)
+    {
+        var summary = BenchmarkRunner.Run<ParsingBenchmarks>();
+    }
+}
