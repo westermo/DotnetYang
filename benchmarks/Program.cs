0cb54534aac11d52849a4d6b2301c7f98d494570
caran <carl.andersson@westermo.com>
2024-06-10 09:11:18 +0200

    Added TraceRoute roundtrip benchmark

--- benchmarks/Program.cs
+++ benchmarks/Program.cs
@@ -2,9 +2,11 @@
 using BenchmarkDotNet.Attributes;
 using BenchmarkDotNet.Running;
 using Ietf.Inet.Types;
+using Yang.Attributes;
 using YangParser;
 using YangParser.Parser;
 using YangParser.SemanticModel;
+using YangSource;
 
 namespace Benchmarks;
 
@@ -17,6 +19,34 @@ public class ParsingBenchmarks
     private IStatement model;
     private Ietf.Bfd.Ip.Mh.YangNode.MultihopNotification notification;
     private string serialized;
+    private IYangServer server;
+    private IChannel channel;
+
+    private static readonly Ietf.Connection.Oriented.Oam.YangNode.TracerouteInput input =
+        new()
+        {
+            MaNameStringValue = new Ietf.Connection.Oriented.Oam.YangNode.TracerouteInput.MaNameString(),
+            MdNameStringValue = new Ietf.Connection.Oriented.Oam.YangNode.TracerouteInput.MdNameString(),
+            Ttl = 2,
+            Count = 4,
+            Interval = 6,
+            CosId = 2,
+            DestinationMep = new Ietf.Connection.Oriented.Oam.YangNode.TracerouteInput.DestinationMepContainer
+            {
+                MepAddress =
+                    new Ietf.Connection.Oriented.Oam.YangNode.TracerouteInput.DestinationMepContainer.MepAddressChoice
+                    {
+                        IpAddressCaseValue =
+                            new Ietf.Connection.Oriented.Oam.YangNode.TracerouteInput.DestinationMepContainer.
+                                MepAddressChoice.IpAddressCaseValueCase
+                                {
+                                    IpAddress = new YangNode.IpAddress(new YangNode.Ipv4Address("1.2.3.4"))
+                                }
+                    }
+            },
+            SourceMepValue = new(),
+            CommandSubType = Ietf.Connection.Oriented.Oam.YangNode.CommandSubTypeIdentity.Proactive
+        };
 
     [GlobalSetup]
     public void Setup()
@@ -31,6 +61,8 @@ public class ParsingBenchmarks
             SourceAddr = new YangNode.IpAddress(new YangNode.Ipv4Address("2.3.4.5"))
         };
         serialized = notification.ToXML().Result;
+        server = new ExampleYangServer();
+        channel = new BenchmarkingChannel(server);
     }
 
     [Benchmark]
@@ -67,6 +99,24 @@ public class ParsingBenchmarks
     {
         using var ms = new MemoryStream(Encoding.UTF8.GetBytes(serialized));
         return await Ietf.Bfd.Ip.Mh.YangNode.MultihopNotification.ParseAsync(ms);
+    }
+
+    [Benchmark]
+    public async Task<Ietf.Connection.Oriented.Oam.YangNode.TracerouteOutput> TracerouteRoundTrip()
+    {
+        return await Ietf.Connection.Oriented.Oam.YangNode.Traceroute(channel, 123, input);
+    }
+}
+
+internal class BenchmarkingChannel(IYangServer server) : IChannel
+{
+    public async Task<Stream> Send(string xml)
+    {
+        using var input = new MemoryStream(Encoding.UTF8.GetBytes(xml));
+        var output = new MemoryStream();
+        await server.Receive(input, output);
+        output.Seek(0, SeekOrigin.Begin);
+        return output;
     }
 }
 
